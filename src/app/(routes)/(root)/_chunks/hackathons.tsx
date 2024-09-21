4968318fe3329a5363bfaea47b0b8f96674f96f4
lakshyaCreates <lakshya.creates07@gmail.com>
2024-09-21 23:32:23 +0530

    remove unused files

--- src/app/(routes)/(root)/_chunks/hackathons.tsx
+++
@@ -1,8 +0,0 @@
-import { Section } from "./section";
-import { info } from "@/info";
-
-export const Hackathons = () => {
-    const data = info.hackathons;
-
-    return <Section heading="hackathons" data={data} />;
-};
