e77becd4ca519058f2a2d53d81b6815a0a38acaa
lakshyaCreates <lakshya.creates07@gmail.com>
2025-02-26 19:23:49 +0530

    feat: change project slug page layout to that of blog slug page

---
+++ src/components/post-thumbnail.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+export const PostThumbnail = ({
+    imageDark,
+    imageLight,
+}: {
+    imageDark: string;
+    imageLight: string;
+}) => {
+    const pathname = usePathname();
+    const slug = pathname.split("/")[1];
+    const { theme } = useTheme();
+
+    return (
+        <Image
+            width={1280}
+            height={800}
+            quality={100}
+            src={theme === "dark" ? imageDark : imageLight}
+            alt={`Blog Thumbnail - ${slug}`}
+            className="aspect-[8/5] min-w-full max-w-full rounded-lg object-cover object-center"
+        />
+    );
+};
